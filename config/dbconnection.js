ad442dbe15837b0620d0ff6e28f9a2f0a5d3f317
Mukeshkumar1611 <mukesh0895.be21@chitkara.edu.in>
2024-09-20 22:03:25 +0530

    Fixed Db Connection

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,11 +1,9 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+require('dotenv').config();  // Make sure dotenv is set up for local development
 
-// Define the PostgreSQL connection parameters
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false, // Set to true if you want to see SQL queries
+    logging: true,
     pool: {
         max: 5,
         min: 0,
